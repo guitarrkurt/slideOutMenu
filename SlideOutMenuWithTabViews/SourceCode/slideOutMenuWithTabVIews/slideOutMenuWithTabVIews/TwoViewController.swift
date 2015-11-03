464aade16b2e17d343a44425455302ef29bf7897
guitarrkurt <guitarrkurt@Cony.local>
2015-11-03 16:13:46 -0600

    slide out menu v1

---
+++ SlideOutMenuWithTabViews/SourceCode/slideOutMenuWithTabVIews/slideOutMenuWithTabVIews/TwoViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TwoViewController.swift
+//  slideOutMenuWithTabVIews
+//
+//  Created by guitarrkurt on 02/11/15.
+//  Copyright © 2015 guitarrkurt. All rights reserved.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
